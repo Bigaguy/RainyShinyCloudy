47081003d618ae987a701861c7129d6ac6dd1580
Andrew Ingram <andrewingram@Andrews-MacBook-Pro.local>
2017-05-16 23:43:41 -0400

    interface and weather data added

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATTITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "a6f59c753cfb486073385bcbf1cd6a42"
+typealias DownloadComplete = () -> ()
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATTITUDE)-4.458\(LONGITUDE)-75.5047\(APP_ID)\(API_KEY)"
--- rainyshinycloudy/WeatherVC.swift
+++ rainyshinycloudy/WeatherVC.swift
@@ -8,16 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var currentWeather = CurrentWeather()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        currentWeather.downloadWeatherDetails {
+            
+            self.updateMainUI()
+            
+        }
+        
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func updateMainUI() {
+        dateLabel.text = currentWeather.date
+        tempatureLabel.text = "\(currentWeather.currentTemp)°C"
+        weatherLabel.text = currentWeather.weatherType
+        locationLabel.text = currentWeather.cityName
+        weatherImage.image = UIImage(named: currentWeather.weatherType)
+        
     }
 
 
